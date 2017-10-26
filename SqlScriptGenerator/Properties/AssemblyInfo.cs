d1ddbe23aaf9859c45d0dbf1863365284c69132a
awhewell <32022822+awhewell@users.noreply.github.com>
2017-10-26 01:58:10 +0100

    Removed dead code

--- SqlScriptGenerator/Properties/AssemblyInfo.cs
+++ SqlScriptGenerator/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SqlScriptGenerator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Generates SQL scripts from database metadata")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SqlScriptGenerator")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © Andrew Whewell 2017 onwards")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
